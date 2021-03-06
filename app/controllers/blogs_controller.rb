2b3cc7b964e195c3e42dc539412a79afa14dd304
24wkt <ec2-user@ip-172-31-14-59.ec2.internal>
2021-03-06 13:23:40 +0000

    [finish]4-3

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,23 @@
+class BlogsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
+  end
+  def edit
+  end
+
+  private
+  def blog_params
+    params.require(:blog).premit(:title,:category,:body)
+  end
+end
